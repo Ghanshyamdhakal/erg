3f56c823e82c8288eb43e25fe34049a2c26cf0f0
Ghanshyamdhakal <70930697+Ghanshyamdhakal@users.noreply.github.com>
2020-09-08 09:29:44 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+     <title>Ghanshyam dhakal persoal Site </title>
+ </head>
+<body>
+<h2 title="I'm a header">The title Attribute</h2>
+
+<p title="I'm a tooltip">Mouse over this paragraph, to display the title attribute as a tooltip.</p>
+
+</body>
+</html>
+
+
+
+
